6acad36d3e954e0a959224768c1b63f3c1539eac
Abakrii <abdelrahmansalembakry@gmail.com>
2019-04-10 19:11:12 +0200

    Working with Arrays Part II

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -16,8 +16,28 @@ const users =[{
     email : "sara"
 },{
     id : "3",
-    name : "Hamdy",
-    email: "Hamdy@Awstreams.com"
+    name : "fil",
+    email: "A@fil.com"
+}]
+
+// Demo Posts Data
+
+const posts =[{
+    id : "1",
+    title : "graphql",
+    body : "this is the first Post",
+    published : true
+},{
+    id: "2",
+    title : "Prisma",
+    body : "this is the secound post",
+    published : true
+
+},{
+    id : "3",
+    title : "react",
+    body: "postingggs",
+    published: false
 }]
 
 
@@ -27,6 +47,7 @@ const typeDefs = `
         users (query: String): [User!]! 
         me: User!
         post: Post!
+        posts (query :String) : [Posts!]!    
     }
 
     type User {
@@ -42,29 +63,48 @@ const typeDefs = `
         body: String!
         published: Boolean!
     }
+
+    type Posts {
+        id : ID!
+        title : String!
+        body: String!
+        published : Boolean!
+    }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        users(parent , ctx , args , info){
-            
+        posts(parent , args , ctx , info){
                 if(!args.query){
-                    return users
+                    return posts
                 }
+                return posts.filter((post)=>{
+                    return (
+                    post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+                      post.body.toLowerCase().includes(args.query.toLowerCase())
+                )})
 
-            return users.filter((user)=>{
-                return user.name.toLowerCase.include(args.query.toLowerCase())
-            }) 
-            
         },
+
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
+            }
+
+            return users.filter((user) => {
+              //  return user.name.toLowerCase().includes(args.query.toLowerCase())
+                return user.id.includes(args.query)
+            })
+        },
+        
         me() {
             return {
                 id: '123098',
                 name: 'Mike',
                 email: 'mike@example.com'
             }
-        },
+        }, 
         post() {
             return {
                 id: '092',
